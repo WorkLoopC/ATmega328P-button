0632bd8efef3a1ed8bc30d5c23f3f7127e83f9a7
Jan Kozel <153216339+WorkLoopC@users.noreply.github.com>
2024-05-13 21:40:00 +0200

    button.c

--- button.c
+++ button.c
@@ -1,14 +1,44 @@
+#include <stdint.h>
 #include <avr/io.h>
 #include <util/delay.h>
-int main(){
-DDRB=0x01;
-PORTB=0x02;
+#include <avr/interrupt.h>
 
-while(1){
-if(!(PINB&0x02)){
-PORTB|=0x01;}
-else{
-PORTB&=0xFE;}
-    }
+volatile uint8_t counter = 0x10; //0b00010000
+
+ISR(INT0_vect){
+	
+	if(counter >= 0x80){
+		counter = 0x10;
+	}else{
+		counter *= 2;
+	}
 }
 
+ISR(INT1_vect){
+	
+	if(counter <= 0x10){
+		counter = 0x80;
+	}else{
+		counter /= 2;
+	}
+}
+
+int main(void)
+{
+	DDRD &= 0xF3; //0b11110011
+	DDRD |= 0xF0; //0b11110000
+	
+	PORTD = 0x0C; //0b00001100
+	
+	EICRA |= (0b10 << ISC10) | (0b10 << ISC00);
+	
+	EIMSK = 0x03; //0b00000011
+	
+	sei();
+	
+    while (1) 
+    {
+		PORTD &= 0x0F; //0b00001111
+		PORTD |= counter; 		
+    }
+}
